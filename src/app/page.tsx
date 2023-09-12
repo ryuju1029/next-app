edf64fc314150545b92293a5f2ea2c283a2738e1
ryuju1029 <ryuju19971029@gmail.com>
2023-09-13 08:43:01 +0900

    feat: csrf token取得の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
-import Header from './components/Header'
- 
+'use client';
 
-export default function Home() {
+import { useEffect } from 'react'
+import axios from 'axios'
+import Header from './components/Header'
+import { CsrfToken } from './types'
+
+export const Home = () => {
+
+  useEffect(() => {
+    axios.defaults.withCredentials = true
+    const getCsrfToken = async () => {
+      const {data} = await axios.get<CsrfToken>(
+        `${process.env.NEXT_PUBLIC_API_URL}/csrf-token`
+      )
+      axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token
+    }
+    getCsrfToken()
+  }, [])
+
+
   return (
     <main className='p-10'>
       <Header />
     </main>
   )
 }
+ 
+export default Home
